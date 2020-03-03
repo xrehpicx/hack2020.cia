da1a1b26bb6c591b19e84c5af3974c198dc833fd
Raj <raj.fps2000@gmail.com>
2020-03-03 14:26:54 +0530

    new schedule

--- js/events.js
+++ js/events.js
@@ -1,84 +1,82 @@
 (function () {
     const events = [
         [{
-            title: 'Check-In Of Hackers Will Start',
+            title: 'Participants registration begin',
             from: '9am',
         },
         {
-            title: 'Opening Ceremony Inauguration',
-            from: '9:30am',
+            title: 'Opening Ceremony',
+            from: '10:30am',
         },
         {
-            title: 'Keynote Session From MLH Representative',
-            from: '10am'
+            title: 'Keynote session',
+            from: '11am'
         },
         {
-            title: 'Keynote Session From non MLH Representative',
-            from: '10:30am'
+            title: 'lunch served',
+            from: '12:30pm'
         },
         {
-            title: 'CIA talk',
-            from: '11:15am'
+            title: 'Workshop 1 & 2 parallely',
+            from: '1:30pm'
         },
         {
-            title: 'Hack Begins!',
-            d: 'Domain experts would provide mentorship at the Hack- All time. Mentors would be interacting with the participants for any technical doubts that they have in there project and with many fun mini-events.',
-            from: '12pm',
-            to: '1:30pm',
+            title: 'hacking strarts',
+            from: '2:30pm',
         },
         {
-            title: 'Workshop on Introduction to Github',
-            from: '2pm',
+            title: 'Workshop 3 & 4 parallely',
+            from: '6:30pm',
         },
         {
-            title: 'Workshop on App Deployment with AWS',
-            from: '3pm',
-            to: '4pm'
+            title: 'dinner served',
+            from: '7:30pm',
         },
-        {
-            title: 'Snack And Beverages Break.',
-            to: '6pm'
 
-        },
-        {
-            title: 'Fireside Chat.',
-            to: '6:30pm'
-        },
-        {
-            title: 'Hack Continues!',
-            to: '9pm',
-            d: 'Hack Continues in its course with many raffle rounds and many fun mini-events.',
-        },
-        {
-            title: 'Dinner Break',
-            to: '10pm'
-        },
-        {
-            title: 'Review 1',
-            from: '12am',
-            to: '1am',
-            d: 'Progress will be noted, and the further plan of action will be recorded.'
-        }
         ],
         [
             {
-                title: ' Presentation Preparation',
-                from: '10am',
-                to: '10:30am',
-                d: 'The top 10 teams will prepare the final presentation to present in front of the whole panel and students.',
+                title: 'midnight snack served alongside campfire',
+                from: '1am',
             },
             {
-                title: 'Review 3 - The Final Presentation',
-                from: '11:30am',
+                title: 'Breakfast',
+                from: '7:30am',
             },
             {
-                title: 'Winners Announcement',
-                from: '3pm',
-                to: '4pm',
-                d: 'The winner will be announced of all domains, and the Hackathon will be concluded with the valedictory ceremony.'
-            }
+                title: 'Slideshow Karaoke',
+                from: '10:30am',
+            },
+            {
+                title: 'Break for lunch',
+                from: '12:30pm',
+            },
+            {
+                title: 'submission ends',
+                from: '2pm',
+            },
+            {
+                title: 'Jugdes briefing starts and demo',
+                from: '2:30pm',
+            },
+            {
+                title: 'scoring and top ten',
+                from: '3:30pm',
+            },
+            {
+                title: 'Top Ten Presentation',
+                from: '4:30pm',
+            },
+            {
+                title: 'Closing Ceremony',
+                from: '5:30pm',
+            },
+            {
+                title: 'Vote of thanks',
+                from: '6:55pm',
+            },
         ]
-    ]
+    ];
     var day1 = document.querySelector('.day1');
     var day2 = document.querySelector('.day2');
     events.forEach((day, ii) => {
