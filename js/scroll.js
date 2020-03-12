35e9e752f7445d5116e94bb6fc8bed97c6ce72a3
Raj <raj.fps2000@gmail.com>
2020-03-12 19:29:30 +0530

    added github logo

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,27 @@
-const elements = [...document.querySelectorAll('.fadein'), ...document.querySelectorAll('.track-card'), ...document.querySelectorAll('.question'), ...document.querySelectorAll('.sponsor'), ...document.querySelectorAll('footer')];
-var observer = new IntersectionObserver(fadein);
-elements.forEach(ele => observer.observe(ele));
-function fadein(entries) {
-    /* console.log(entries) */
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) entry.target.style.opacity = '1';
-        else entry.target.style.opacity = '0';
-    });
-}
+(() => {
+    const elements = [document.querySelectorAll('.fadein'), document.querySelectorAll('.track-card'), document.querySelectorAll('.question'), document.querySelectorAll('.sponsor'), document.querySelectorAll('footer')];
+    /* 
+    var observer = new IntersectionObserver(fadein);
+    
+    function fadein(entries) {
+     
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) entry.target.style.opacity = '1';
+            else entry.target.style.opacity = '0';
+        });
+    } */
 
+    Promise.all(elements.map(eles => {
+        return new Promise((resolve, reject) => {
+            const observer = new IntersectionObserver(fadein);
+            function fadein(entries) {
+                entries.forEach(entry => {
+                    if (entry.intersectionRatio > 0) entry.target.style.opacity = '1';
+                    else entry.target.style.opacity = '0';
+                });
+            }
+            eles.forEach(e => observer.observe(e));
+            resolve('done');
+        })
+    }));
+})
