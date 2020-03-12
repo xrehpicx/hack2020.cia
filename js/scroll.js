a53fd9b7a778b0f94bbf03f8d6f2988fa74dbd79
Raj <raj.fps2000@gmail.com>
2020-03-12 09:40:02 +0530

    tried fixing schedule items not appearing sometimes

--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,12 @@
-(() => {
-    
-    const elements = [...document.querySelectorAll('.fadein'), ...document.querySelectorAll('.track-card'), ...document.querySelectorAll('.question'), ...document.querySelectorAll('.sponsor'), ...document.querySelectorAll('footer')];
-    /* console.log(elements); */
-    let observer = new IntersectionObserver(fadein);
-    
-    function fadein(entries) {
-        /* console.log(entries) */
-        entries.forEach(entry => {
-            if (entry.intersectionRatio > 0) entry.target.style.animation = `fadein 200ms forwards ease-in`;
-            else entry.target.style.animation = 'none';
-        });
-    }
+const elements = [...document.querySelectorAll('.fadein'), ...document.querySelectorAll('.track-card'), ...document.querySelectorAll('.question'), ...document.querySelectorAll('.sponsor'), ...document.querySelectorAll('footer')];
+var observer = new IntersectionObserver(fadein);
 
-    elements.forEach(ele => observer.observe(ele));
-})();
+function fadein(entries) {
+    /* console.log(entries) */
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) entry.target.style.animation = `fadein 200ms forwards ease-in`;
+        else entry.target.style.animation = 'none';
+    });
+}
+
+elements.forEach(ele => observer.observe(ele));
