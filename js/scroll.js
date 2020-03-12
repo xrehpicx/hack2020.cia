6dcec0dfe343458294b7d3b8c1e2d76522f6d1ab
Raj <raj.fps2000@gmail.com>
2020-03-12 09:55:25 +0530

    tried fixing fadein

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,11 @@
 const elements = [...document.querySelectorAll('.fadein'), ...document.querySelectorAll('.track-card'), ...document.querySelectorAll('.question'), ...document.querySelectorAll('.sponsor'), ...document.querySelectorAll('footer')];
 var observer = new IntersectionObserver(fadein);
-
+elements.forEach(ele => observer.observe(ele));
 function fadein(entries) {
     /* console.log(entries) */
     entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) entry.target.style.animation = `fadein 200ms forwards ease-in`;
-        else entry.target.style.animation = 'none';
+        if (entry.intersectionRatio > 0) entry.target.style.opacity = '1';
+        else entry.target.style.opacity = '0';
     });
 }
 
-elements.forEach(ele => observer.observe(ele));
